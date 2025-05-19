fa051ee243ddc464fd542545c32d1dabb9f01c36
tusharrr622 <tusharraj622@gmail.com>
2025-05-19 17:10:26 +0530

    cors file

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,22 @@ const Task = require('./models/Task');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'saesgrfsg415sr';
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://task-manager-j1lb.vercel.app'
+];
+
+app.use(cors({
+    credentials: true,
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
